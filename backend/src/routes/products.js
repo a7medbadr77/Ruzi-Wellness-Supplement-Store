76f1dd110ff02e0fe7fd463ca06fa1b7001c097d
a7medbadr77 <abader600@live.com>
2026-03-03 22:22:56 +0200

    Create products.js

---
+++ backend/src/routes/products.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const productController = require('../controllers/productController');
+
+const router = express.Router();
+
+router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProductById);
+router.get('/category/:category', productController.getProductsByCategory);
+
+module.exports = router;
